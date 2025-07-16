87153b495d9b9b97cedeb4064302b5c6bc88b009
bitto149 <bitto1497@gmail.com>
2025-07-16 19:15:19 +0200

    Add files via upload

--- text-to-speech.js
+++ text-to-speech.js
@@ -1,8 +1,1 @@
-function speak(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'it-IT';
-    utterance.pitch = 1;
-    utterance.rate = 1;
-    utterance.voice = speechSynthesis.getVoices().find(voice => voice.lang === 'it-IT');
-    speechSynthesis.speak(utterance);
-}
+function speak(text){ const u = new SpeechSynthesisUtterance(text); speechSynthesis.speak(u); }
